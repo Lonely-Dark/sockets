2ff2bb4cbe99d32774512912f8bd702d05866deb
Vladdg35 <vlad10game@gmail.com>
2018-12-22 18:06:18 +0300

    ADD foders

---
+++ server/sockets.py
@@ -0,0 +1,19 @@
+import socket 
+import time
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port=8080
+s.bind(('localhost', port))
+s.listen(2)
+print("Server start!")
+print("Initialization,wait")
+time.sleep(1)
+print("Waiting for connection")
+while True:
+	conn, addr = s.accept()
+	data=conn.recv(1024)
+	print("Connected: ", addr)
+	print(data)
+	conn.send(b'Text message')
+	conn.close()
+	time.sleep(0.1)
+
